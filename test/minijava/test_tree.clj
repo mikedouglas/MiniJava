f39cf42db1ce17b362ea5aa0a222bc3a760af7a5
Mike Douglas <mike.douglas@gmail.com>
2010-03-04 16:17:59 -0800

    More cleanup.

--- test/minijava/test_tree.clj
+++ test/minijava/test_tree.clj
@@ -1,61 +1,56 @@
 (ns minijava.test-tree
   (:use clojure.test
-        (minijava ast label tree utility))
-  (:require [minijava.ir :as ir]))
-
-(import-ast-classes)
+        (minijava ast label ir tree utility)))
 
 (deftest tests-binop-conv
-  (let [plus  (parse-exp "5 + 5")
-        minus (parse-exp "5 - 5")
-        times (parse-exp "5 * 5")
-        and   (parse-exp "5 && 5")
-        less-than (parse-exp "5 < 5")
-        matches (fn [op ast] (= (ir/BinaryOp op (ir/Const 5) (ir/Const 5))
+  (let [plus   (parse-exp "5 + 5")
+        minus  (parse-exp "5 - 5")
+        times  (parse-exp "5 * 5")
+        and    (parse-exp "5 && 5")
+        l-than (parse-exp "5 < 5")
+        matches (fn [op ast] (= (BinaryOp op (Const 5) (Const 5))
                                 (tree ast)))]
     (dorun
      (map #(is (matches %1 %2) "AST converts to BinaryOp IR.")
-          [:+   :-    :*    :&&  :<]
-          [plus minus times and less-than]))))
+          [:+   :-    :*    :&& :<]
+          [plus minus times and l-than]))))
 
 (deftest tests-int-bool-conv
-  (is (= (ir/Const 5) (tree (parse-exp "5"))))
-  (is (= (ir/Const 1) (tree (parse-exp "true"))))
-  (is (= (ir/Const 0) (tree (parse-exp "false")))))
+  (is (= (Const 5) (tree (parse-exp "5"))))
+  (is (= (Const 1) (tree (parse-exp "true"))))
+  (is (= (Const 0) (tree (parse-exp "false")))))
 
 (deftest tests-special-if
   (is (= (let [t (label)
                f (label)]
-           (ir/Seq [(ir/Conditional :< (ir/Const 3) (ir/Const 4)
-                                    (ir/Name t) (ir/Name f))
-                    (ir/Label t)
-                    (ir/Seq [])
-                    (ir/Label f)
-                    (ir/Seq [])]))
-          (tree (parse-stm "if (3 < 4) {} else {}")))
+           (Seq [(Conditional :< (Const 3) (Const 4) (Name t) (Name f))
+                 (Label t)
+                 (Seq [])
+                 (Label f)
+                 (Seq [])]))
+         (tree (parse-stm "if (3 < 4) {} else {}")))
       "If statements with boolean tests convert correctly."))
 
 (deftest tests-regular-if
   (is (= (let [t (label)
                f (label)]
-           (ir/Seq [(ir/Conditional :!= (ir/BinaryOp :+ (ir/Const 3) (ir/Const 4))
-                                    0 (ir/Name t) (ir/Name f))
-                    (ir/Label t)
-                    (ir/Seq [])
-                    (ir/Label f)
-                    (ir/Seq [])]))
+           (Seq [(Conditional :!= (BinaryOp :+ (Const 3) (Const 4))
+                              0 (Name t) (Name f))
+                 (Label t)
+                 (Seq [])
+                 (Label f)
+                 (Seq [])]))
          (tree (parse-stm "if (3 + 4) {} else {}")))
       "If statements that test normal expressions convert correctly."))
 
 (deftest tests-while
-  (is (= (let [test  (label)
-               t (label)
-               f (label)]
-           (ir/Seq [(ir/Label test)
-                    (ir/Conditional :< (ir/Const 3) (ir/Const 4)
-                                    (ir/Name t) (ir/Name f))
-                    (ir/Label t)
-                    (ir/Seq [])
-                    (ir/Jump test)
-                    (ir/Label f)])))
+  (is (= (let [test (label)
+               t    (label)
+               f    (label)]
+           (Seq [(Label test)
+                 (Conditional :< (Const 3) (Const 4) (Name t) (Name f))
+                 (Label t)
+                 (Seq [])
+                 (Jump test)
+                 (Label f)])))
       "While statements convert correctly."))
