018da9574916e9c001c2f0d85befb625e2f8608d
Sam <sam@ubuntu.ubuntu-domain>
2010-03-07 23:20:22 -0800

    Syncing with changes uploaded to git...

--- src/clj/minijava/interp.clj
+++ src/clj/minijava/interp.clj
@@ -1,33 +1,41 @@
 (ns minijava.interp
   (:use (minijava ir))
   (:import (minijava.ir.temp.label)))
-
+ 
 (defstruct env :temps :mem :labels)
-
-(def empty-env 
+ 
+(def empty-env
   (atom (struct env (hash-map) (hash-map) (hash-map))))
-
+ 
 (defn write-temp [env key val]
   (swap! env (fn [e k v] (assoc-in e [:temps k] v)) key val))
 (defn read-temp [env key]
   (get-in @env [:temps key]))
 (defn read-label [env key]
   (get-in @env [:labels key]))
-
+ 
+(comment eval-ir evaluates the linearized IR tree. Only types that
+         appear in the final tree have cases in the multimethod.
+         e.g. ESeq and Seq are gone after linearization)
+ 
 (comment dispatch on type of first arg)
 (defmulti eval-ir (fn [x y] (type x)))
-
+ 
+(comment this gets called if the program jumps to the very end)
+(defmethod eval-ir clojure.lang.PersistentList$EmptyList [lst env]
+  nil)
+ 
 (comment use lookahead to see if we jump or evaluate normally)
 (defmethod eval-ir clojure.lang.PersistentList [lst env]
-  (cond (empty? (rest lst)) ;; since dispatching on empty is annoying
+  (cond (or (= (type (first lst)) :minijava.ir/Jump)
+            (= (type (first lst)) :minijara.ir/Conditional))
           (eval-ir (first lst) env)
-        (or (isa? (first lst) :minijava.ir/Jump)
-            (isa? (first lst) :minijara.ir/Conditional))
+        (empty? (rest lst))
           (eval-ir (first lst) env)
-        true 
+        true
           (do (eval-ir (first lst) env)
               (eval-ir (rest lst) env))))
-
+ 
 (defmethod eval-ir :minijava.ir/BinaryOp [exp env]
   (let [e1 (eval-ir (:exp1 exp) env)
         e2 (eval-ir (:exp2 exp) env)]
@@ -35,49 +43,49 @@
           :+ (+ e1 e2)
           :- (- e1 e2)
           :* (* e1 e2))))
-
+ 
 (defmethod eval-ir :minijava.ir/Const [exp env]
   (:val exp))
-
+ 
 (defmethod eval-ir :minijava.ir/Conditional [exp env]
   (let [e1 (eval-ir (:exp1 exp) env)
-        e2 (eval-ir (:exp2 exp) env)]
+        e2 (eval-ir (:exp2 exp) env)
+        lt (read-label env (:lbl (:t exp)))
+        lf (read-label env (:lbl (:f exp)))]
     (case (:op exp)
-          :<  (if (< e1 e2) 1 0)
-          :!= (if (not (= e1 e2)) 1 0)
-          :=  (if (= e1 e2) 1 0))))
-
-; Canonicalization removes eseqs and also seqs
-;
-;(defmethod eval-ir ::minijava.ir/ExpSeq [exp]
-;  (let [e (eval-ir (:exp exp))
-;          (eval-ir (:seqs exp))]
-;    (...)))
-;
-;(defmethod eval-ir ::minijava.ir/Seq [exp])
-
+          :< (if (< e1 e2)
+                  (eval-ir lt env)
+                  (eval-ir lf env))
+          :!= (if (not (= e1 e2))
+                  (eval-ir lt env)
+                  (eval-ir lf env))
+          := (if (= e1 e2)
+                  (eval-ir lt env)
+                  (eval-ir lf env)))))
+          
+ 
 (defmethod eval-ir ::minijava.ir/Move [exp env]
   (let [val (eval-ir (:src exp) env)
         dst (:reg (:dst exp))]
     (write-temp env dst val)))
-
+ 
 (defmethod eval-ir ::minijava.ir/Jump [exp env]
   (eval-ir (read-label env (:lbl exp)) env))
-
-(comment Labels don't do anything after the label table is
-         built)
+ 
+(comment Labels and names don't do anything after the label
+         table is built. These should never be called since
+         eval-ir unpacks these directly.)
 (defmethod eval-ir ::minijava.ir/Label [exp env]
   nil)
-
-(defmethod eval-ir ::minijava.ir/Mem [exp env]
-  nil)
-
 (defmethod eval-ir ::minijava.ir/Name [exp env]
   nil)
-
+ 
+(defmethod eval-ir ::minijava.ir/Mem [exp env]
+  nil)
+ 
 (defmethod eval-ir ::minijava.ir/Temp [exp env]
   (read-temp env (:reg exp)))
-
+ 
 (comment Build map of label code - should be efficient by persistence
          of list data structure)
 (defn build-label-map [stms map]
@@ -86,9 +94,10 @@
           (build-label-map (rest stms)
                            (assoc map (:lbl (first stms)) (rest stms)))
         true (build-label-map (rest stms) map)))
-
+ 
 (comment top-level eval for a sequence of statements)
 (defn eval-prog [stms]
   (let [labels (build-label-map stms (hash-map))]
-    (eval-ir stms 
+    (eval-ir stms
              (atom (struct env (hash-map) (hash-map) labels)))))
+ 
--- test/minijava/test_interp.clj
+++ test/minijava/test_interp.clj
@@ -1,31 +1,31 @@
 (ns minijava.test-interp
   (:use clojure.test
         (minijava ast interp label tree utility x86))
-  (:import (minijava.ir.temp.label))
+  ;;(:import (minijava.ir.temp.label))
   (:require [minijava.ir :as ir]))
-
+ 
 (import-ast-classes)
-
+ 
 (def empty-frame (new-x86 0 ["obj"]))
-
+ 
 (deftest test-eval-const
   (let [const (tree (parse-exp "5") empty-frame)]
     (is (= 5 (eval-ir const empty-env)))))
-
+ 
 (deftest test-eval-binop
-  (let [plus  (tree (parse-exp "5 + 5") empty-frame)
+  (let [plus (tree (parse-exp "5 + 5") empty-frame)
         minus (tree (parse-exp "5 - 5") empty-frame)
         times (tree (parse-exp "5 * 5") empty-frame)]
     (is (= 10 (eval-ir plus empty-env)))
-    (is (= 0  (eval-ir minus empty-env)))
+    (is (= 0 (eval-ir minus empty-env)))
     (is (= 25 (eval-ir times empty-env)))))
-
+ 
 (deftest test-temp
   (let [tmp (minijava.ir.temp.Temp.)
         prog (list (ir/Move (ir/Const 5) (ir/Temp tmp))
                    (ir/Temp tmp))]
     (is (= 5 (eval-prog prog)))))
-
+ 
 (deftest test-labels
    (let [t (label)
          prog (list (ir/Const 7)
@@ -33,7 +33,7 @@
                     (ir/Const 5))]
      (is (= (build-label-map prog (hash-map))
             (hash-map t (list (ir/Const 5)))))))
-
+ 
 (deftest test-jump
    (let [t (label)
          tmp (minijava.ir.temp.Temp.)
@@ -42,10 +42,45 @@
                     (ir/Move (ir/Const 5) (ir/Temp tmp))
                     (ir/Label t)
                     (ir/Temp tmp))]
-     (is (= 5 (eval-prog prog)))
+     (is (= 0 (eval-prog prog)))
      (is (= (build-label-map prog (hash-map))
             (hash-map t (list (ir/Temp tmp)))))))
-
+ 
+(deftest test-while
+  (let [t (label)
+        f (label)
+        s (label)
+        tmp (minijava.ir.temp.Temp.)
+        prog (list (ir/Move (ir/Const 0) (ir/Temp tmp))
+                   (ir/Label s)
+                   (ir/Conditional :< (ir/Temp tmp) (ir/Const 10)
+                       (ir/Name f) (ir/Name t))
+                   (ir/Label t)
+                   (ir/Move (ir/BinaryOp :+ (ir/Temp tmp) (ir/Const 1))
+                            (ir/Temp tmp))
+                   (ir/Jump s)
+                   (ir/Label f)
+                   (ir/Temp tmp))]
+    (is (= 10 (eval-prog prog)))
+    (is (= (build-label-map prog (hash-map))
+           (hash-map s
+             (list (ir/Conditional :< (ir/Temp tmp) (ir/Const 10)
+                       (ir/Name t) (ir/Name f))
+                   (ir/Label t)
+                   (ir/Move (ir/BinaryOp :+ (ir/Temp tmp) (ir/Const 1))
+                            (ir/Temp tmp))
+                   (ir/Jump s)
+                   (ir/Label f)
+                   (ir/Temp tmp))
+             t
+             (list (ir/Move (ir/BinaryOp :+ (ir/Temp tmp) (ir/Const 1))
+                            (ir/Temp tmp))
+                   (ir/Jump s)
+                   (ir/Label f)
+                   (ir/Temp tmp))
+             f
+             (list (ir/Temp tmp)))))))
+ 
 (comment Sanity checks)
 (deftest test-label
   (let [t (label)
@@ -54,3 +89,4 @@
            (first prog)))
     (is (= (hash-map t 5)
            (hash-map (:lbl (first prog)) 5)))))
+ 
