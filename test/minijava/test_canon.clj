47da6c0259d825a4c9b16f8820d1748179a2bd67
Mike Douglas <mike.douglas@gmail.com>
2010-03-08 20:21:17 -0800

    Fixes on canonicalization algorithm.

--- test/minijava/test_canon.clj
+++ test/minijava/test_canon.clj
@@ -20,8 +20,22 @@
            3))))
 
 (deftest test-remove-eseq-commute
-  (let [exp (BinaryOp :+ (ExpSeq [(Statement (Const 0))] (Const 2))
-                      (Const 3))
+  (let [exp (BinaryOp :+ (Const 2) (ExpSeq [(Statement (Const 0))] (Const 3)))
         result [(Statement (Const 0)) (BinaryOp :+ (Const 2) (Const 3))]]
     (is (= (remove-eseq-commute exp)
-           result))))
+           result)
+        "Removes ExpSeq per rule three.")
+    (is (= (eval-prog (seq result))
+           5))))
+
+(deftest test-remove-eseq-no-commute
+  (let [exp (BinaryOp :+ (Const 3) (ExpSeq [(Statement (Const 0))] (Const 2)))
+        t (minijava.ir.temp.Temp.)
+        result [(Move (Const 3) (Temp t))
+                (Statement (Const 0))
+                (BinaryOp :+ (Temp t) (Const 2))]]
+    (is (= (remove-eseq-no-commute exp)
+           (seq result))
+        "Removes ExpSeq per rule four.")
+    (is (= (eval-prog (seq result))
+           5))))
