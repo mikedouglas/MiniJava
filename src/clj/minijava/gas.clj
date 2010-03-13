6ef9582898bb62d95bf84ffe3075a07ee60378c3
Sam <sam@ubuntu.ubuntu-domain>
2010-03-13 12:02:48 -0800

    Implemented many more x86 instructions

--- src/clj/minijava/gas.clj
+++ src/clj/minijava/gas.clj
@@ -20,4 +20,35 @@
  (deftype imul [src dst]
   clojure.lang.IPersistentMap)
   
+  ;;This pushes what would be the next value for %eip onto the stack, and jumps to the destination address. Used for function calls.
+  (deftype call [dst]
+    clojure.lang.IPersistentMap)
+    
+  ;;An unconditional jump. This simply sets %eip to the destination address.
+  (deftype jmp [dst]
+      clojure.lang.IPersistentMap)
+
+;;Compares two integers. It does this by subtracting the first operand from the second. 
+;;It discards the results, but sets the flags accordingly. Usually used before a conditional jump.
+(deftype cmpl [a b]
+ clojure.lang.IPersistentMap)
+
+;;Conditional branch. cc is the condition code. Jumps to the given address if the condition code is true (set from the previous instruction, probably a comparison). Otherwise, goes to the next instruction. 
+;;The condition codes are (code, effect):
+;; :g >
+;; :ge >=
+;; :l <
+;; :le <=
+;; :e ==
+;; :ne !=
+
+;;There are more we could use, but these are likely enough.
+;;Note that 'jcc' is itself not an x86 instruction. The actual instruction will be 'jge', if cc is set to ge.
+(deftype jcc [cc dst]
+ clojure.lang.IPersistentMap)
+
+;;Pops a value off of the stack and then sets %eip to that value. Used to return from function calls.
+(deftype ret []
+   clojure.lang.IPersistentMap)
+
   
