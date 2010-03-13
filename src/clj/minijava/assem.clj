932d5b1f536a6bf90b4e2000f874f0df30244b53
Asumu Takikawa <asumu@interchange.ubc.ca>
2010-03-13 14:28:46 -0800

    Added some interfaces to the assem classes I forgot

--- src/clj/minijava/assem.clj
+++ src/clj/minijava/assem.clj
@@ -8,18 +8,24 @@
   (format [this]))
 
 (deftype AssemOper [fmt, dsts, srcs, jmps]
+  clojure.lang.IPersistentMap
+  Instr
   (use [] srcs)
   (def [] dsts)
   (jumps [] jmps)
   (format [] "unimplemented"))
 
 (deftype AssemMove [fmt, dst, src]
+  clojure.lang.IPersistentMap
+  Instr
   (use [] (list src))
   (def [] (list dst))
   (jumps [] '())
   (format [] "unimplemented"))
 
 (deftype AssemLabel [fmt, lbl]
+  clojure.lang.IPersistentMap
+  Instr
   (use [] '())
   (def [] '())
   (jumps [] '())
