203963d62fe10f3b380470127b8bbf2f414de027
Sam <sam@ubuntu.ubuntu-domain>
2010-03-26 13:27:05 -0700

    Test cases for flow.clj (everything seems to be working so far)

--- src/clj/minijava/flow.clj
+++ src/clj/minijava/flow.clj
@@ -26,14 +26,20 @@
 			  (assoc succMap mapKey updated)))
 
 ;;create a map linking each instruction to a list of successors
+;;Note: This assumes that there are never two consecutive labels (a situation which could be simplified to just one label anyhow).
+;;Note: Even though LABELS are not real instructions, this defines successors for them. They'll be ignored by liveness though.
+;;The last instruction in the program will be expected to map to nil, since it has not successor.
 (defn flow [program]
 (let [labelMap (mapLabels program)]	 ;;for each label, determine what its following instruction is. This avoids repeated linear time searches later on.
 		(loop		[instrs program
 						 succMap (hash-map)]		
+		 (if (empty? instrs)
+		 			succMap 
 		 (let [hasNext (not (isit?  (first instrs) :minijava.gas/jmp)) ;;for anything except an unconditional jump, the next instruction is a successor
 					hasLabel (or (isit?  (first instrs) :minijava.gas/jmp) (isit?  (first instrs) :minijava.gas/jcc))
 					succMap (if hasNext (addSuccessor succMap (first instrs) (second instrs)) succMap)
 					succMap (if hasLabel (addSuccessor succMap (first instrs) (get labelMap (:dst (first instrs)))) succMap)]
 									
 					(recur (rest instrs) succMap)		
+					)
 ))))
