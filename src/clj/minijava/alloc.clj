4be9c5d106f23c39d42f327edf41eac5704f6649
Mike Douglas <mike.douglas@gmail.com>
2010-03-30 16:02:18 -0700

    Skeleton for register alloc code.

---
+++ src/clj/minijava/alloc.clj
@@ -0,0 +1,19 @@
+(ns minijava.alloc
+  "Register allocation, using a linear scan algorithm.
+   See Poletto and Sarkar[1999]"
+  (:use [minijava flow gas]))
+
+(defn scan
+  "Scan through register intervals, allocating a register/stack pointer
+to each."
+  [intrvls]
+  (let [active {}]
+    ))
+
+(defn- expire
+  "Expire registers that've been freed from intrvl and intrvl - 1."
+  [intrvl active])
+
+(defn- spill
+  "Spill register with longest time left."
+  [intrvl active])
---
+++ test/minijava/test_alloc.clj
@@ -0,0 +1,2 @@
+(ns minijava.test-alloc
+  (:use [minijava alloc]))
