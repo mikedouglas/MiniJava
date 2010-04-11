ddd757d1e909517ae6329f5ea2e7c3e99e590137
Mike Douglas <mike.douglas@gmail.com>
2010-04-11 13:13:21 -0700

    First attempt at register allocation. Untested.

--- src/clj/minijava/alloc.clj
+++ src/clj/minijava/alloc.clj
@@ -5,17 +5,37 @@
 
 (declare expire spill)
 
+(def regs #{:EAX :EBX :EDX})
+
+;; liveness interval {:reg REGS, :loc bool, :start lineno, :end lineno}
 (defn scan
   "Scan through register intervals, allocating a register/stack pointer
-to each."
+to each. Returns allocated intervals."
   [intrvls]
-  (let [active {}]
-    ))
+  (let [active (atom '())
+        spilled (for [i (sort-by :start intrvals)]
+                  (expire i active)
+                  (if (= (count @active) (count regs))
+                    (spill i active)
+                    (let [reg (first (clojure.set/difference regs @active))
+                          i   (assoc i :reg reg)]
+                      (swap! active conj i)
+                      nil)))]
+    {:active active, :spilled (remove nil? spilled)}))
 
 (defn- expire
   "Expire registers that've been freed from intrvl and intrvl - 1."
-  [intrvl active])
+  [intrvl active]
+  (for [a (sort-by :end @active)
+          :when (>= (:end a) (:start i))]
+    a))
 
 (defn- spill
   "Spill register with longest time left."
-  [intrvl active])
+  [intrvl active]
+  (let [spill (first @active)]
+    (if (> (:end spill) (:end intrvl))
+      (let [intrvl (assoc intrvl :reg (:reg spill))]
+        (reset! active (conj (rest @active) intrvl))
+        intrvl)
+      (assoc intrvl :loc true))))
