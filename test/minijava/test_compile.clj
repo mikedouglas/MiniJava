52244c9eccfc64ad3186562dad9e6f321111b67d
Sam <sam@sam-ubuntu.(none)>
2010-04-19 17:32:13 -0700

    Added a factorial test case.

--- test/minijava/test_compile.clj
+++ test/minijava/test_compile.clj
@@ -3,9 +3,40 @@
         clojure.contrib.def
         (minijava compile)))
 
+(comment
 (deftest tests-files-convert-without-exception
   (let [filter (proxy [java.io.FilenameFilter] []
                  (accept [_ name] (not (nil? (re-find #"java$" name)))))
         files (-> "resources/sample" java.io.File. (.listFiles filter))]
     (doseq [f files]
-      (compile-program f))))
+   (println (compile-program f)))))
+
+)
+
+
+
+(deftest test-factorial
+	(let [prog 
+
+"class Factorial{
+	public static void main(String[] a){
+		System.out.println(new Fac().ComputeFac(10));
+	}
+}
+
+class Fac {
+
+	public int ComputeFac(int num){
+		int num_aux ;
+		if (num < 1)
+			num_aux = 1 ;
+		else 
+			num_aux = num * (this.ComputeFac(num-1)) ;
+		return num_aux ;
+	}
+
+}" ]
+
+ (println (compile-program prog))
+
+))
