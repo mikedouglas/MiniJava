e8f292ac42c261b0bf1f66dcbb10461dab348ae2
Mike Douglas <mike.douglas@gmail.com>
2010-04-19 18:06:13 -0700

    Compile up to trace.

--- src/clj/minijava/compile.clj
+++ src/clj/minijava/compile.clj
@@ -7,40 +7,36 @@
 
 
 (defn apply-canon-helper [keyset originalmap newmap]
-	(if (empty? keyset) newmap
-		;;else
-			(let [methodKey (first keyset)
-						methodPair (get originalmap methodKey)
-						newIR  (canon (:ir methodPair))
-						newMethodPair (merge methodPair [:ir newIR])]
-					(apply-canon-helper (rest keyset) originalmap (assoc newmap methodKey newMethodPair))
-			)
-	)
-)
+  (if (empty? keyset) newmap
+      ;;else
+      (let [methodKey (first keyset)
+            methodPair (get originalmap methodKey)
+            newIR  (canon (:ir methodPair))
+            newMethodPair (merge methodPair [:ir newIR])]
+        (apply-canon-helper (rest keyset) originalmap
+                            (assoc newmap methodKey newMethodPair)))))
 
 ;;for each mapping functionName->(IRcode frame), replace it with the mapping
 ;;functionName->((canon IRcode) frame)
 (defn apply-canon [treemap]
-	(apply-canon-helper (keys treemap) treemap (hash-map))
-)
+  (apply-canon-helper (keys treemap) treemap (hash-map)))
 
 ;; an HOF that applies the given transformation to the method's data
 ;; (map-method canon map) should do the same as apply-canon above
 (defn map-method [f treemap]
-  (apply hash-map 
-    (mapcat
-      (fn [pair] 
+  (into {}
+    (map
+      (fn [pair]
         (let [methodName (first pair)
               val        (second pair)
               frame      (:frame val)
               data       (:ir val)]
-          [key, {:frame frame, :ir (f data)}]))
+          [methodName, {:frame frame, :ir (f data)}]))
       treemap)))
 
 (defn compile-program [program]
   (let [treemap   (apply-tree (parse program))
         canonmap  (map-method canon treemap)
         blocksmap (map-method basic-blocks canonmap)
-        munchmap  (map-method (fn [blocks] (map select blocks)) 
-                              blocksmap)]
-    munchmap))
+        tracemap  (map-method #(trace % nil) blocksmap)]
+    tracemap))
